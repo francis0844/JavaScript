0aacf37b8e929a00ee92110b9d2ae95b345e9ab1
francis123 <braposas84@gmail.com>
2023-08-07 11:43:59 +0800

    JavaScript Files 5

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-// Common String Methods
-let email = "braposas84@gmail.com";
+let radius = 10;
+const pi = 3.14;
 
-// let result = email.lastIndexOf('o'); // Last letter that is use on the parameter
+// console.log(radius, pi);
 
-// let result = email.slice(0, 9); // Slicing the word depends on the parameters
+// Math oprators (+, -, *, /, **, %)
 
-// let result = email.substr(4, 10); // Same as slice
+// console.log(10 /2);
+// let result = radius % 3; //Remainder
+// let result = pi * radius**2
 
-let result = email.replace('b', 'R'); // Replacing String (only the first letter encountered)
+// order of operation - BIDMAS - brackets, indecies, division, multiplication, addition, subtraction
+
+// let result = 5 * (10-3)**2
+
+// console.log(result);
+
+let likes = 10;
+// likes = likes + 1; //There is a shorter version for this
+// likes++; // This is the same in adding one
+// likes--; // Same goes with subtraction
+
+// Adding 10
+// likes += 10; // Add 10 (same goes with other op)
+// likes -= 5;
+// likes *= 2;
+// likes /=2;
+
+// console.log(likes);
+
+// NaN = not a number
+// console.log(5 / "hello"); // hello is NaN - not a number
+
+// Number concatenation with string
+let result = "The blog has " + likes + ' likes'
 
 console.log(result);
