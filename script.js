c5a9ab7afd98d4e67e743eae5f48efb62efe6ac5
francis123 <braposas84@gmail.com>
2023-08-07 11:07:28 +0800

    JavaScript Files 2

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log(1);
-console.log(2);
+let age = 25; // two words that are separated with space cannot be used as a name, you can't also use variable assigner as a name, names cannot also start with number
+let year = 2019;
+
+// This is how a comment looks like
+
+// Log things to console
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100; // Const cannot be overridden like how let can be overridden
+console.log(points);
+
+var score = 75; // Older way of creating variables
+console.log(score);
+
+// Modern JavaScript is sometimes not compatible with older browsers when using let and const variable assigner
