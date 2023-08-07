a81d999a008da5e39a348f7e44a0faa780b54545
francis123 <braposas84@gmail.com>
2023-08-07 13:55:17 +0800

    JavaScript Files 21

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-// switch
+// variables & block scope
 
+let age = 30; // Global score - it can be accessed anywhere in the file
 
-// Evaluating Variable
-const grade = 'D'; //Switch case are checking very strictly
-
-switch(grade){
-    case 'A':
-        console.log('You got A');
-        break;
-    case 'B':
-        console.log('You got B');
-        break;
-    case 'C':
-        console.log('You got C');
-        break;
-    case 'D':
-        console.log('You got D');
-        break;
-    case 'E':
-        console.log('You got E');
-        break;
-    default:
-        console.log('not a grade');
+if(true){
+    let age = 40; // Redifining variable inside a code block will be returned only by what is inside the cide block
+    let name = 'shaun'; //Only accessible inside a code block (local scope)
+    console.log("inside if: ", age, name );
 }
+
+console.log("outside if: ", age, name );
