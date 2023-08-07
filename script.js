139b61c8d806d85566350877dee7c5b7ef0da042
francis123 <braposas84@gmail.com>
2023-08-07 16:15:27 +0800

    DOM 4

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-const para = document.querySelector('p');
+const link = document.querySelector('a');
 
-console.log(para.innerText);
-para.innerText = 'I am a ninja';
+console.log(link.getAttribute('href'));
 
-const paras = document.querySelectorAll('p');
+link.setAttribute('href', 'https://youtube.com');
+link.innerText = 'Youtube';
 
-paras.forEach(para => {
-    console.log(para.innerText);
-    para.innerText += ' new text';
-})
+const msg = document.querySelector('p');
 
-const content = document.querySelector('.content');
-
-console.log(content.innerHTML);
-content.innerHTML += '<h2>This is a new h2</h2>';
-
-const people = ['mario', 'luigi', 'yoshi'];
-
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`
-});
+console.log(msg.getAttribute('class'));
+msg.setAttribute('class', 'success');
+msg.setAttribute('style', 'color: red');
