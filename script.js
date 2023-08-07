575aeaa5fdb3b21defce9abe871af8eb20912fbf
francis123 <braposas84@gmail.com>
2023-08-07 14:04:25 +0800

    JavaScript Files 22

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// variables & block scope
-
-let age = 30; // Global score - it can be accessed anywhere in the file
-
-if(true){
-    let age = 40; // Redifining variable inside a code block will be returned only by what is inside the cide block
-    let name = 'shaun'; //Only accessible inside a code block (local scope)
-    console.log("inside if: ", age, name );
+// Function declaration - This can still run even it comes after when we call it
+function greet(){
+    console.log('hello there');
 }
 
-console.log("outside if: ", age, name );
+// Function expression - This will not run if called after the code block itself
+const speak =  function(){
+    console.log('good day!');
+};
+
+greet();
+speak();
