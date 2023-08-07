89eb18bcda4c304b578407b8879183b44ddb1c15
francis123 <braposas84@gmail.com>
2023-08-07 11:31:01 +0800

    JavaScript Files 4

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
-// Strings
-console.log('hello, world');
+// Common String Methods
+let email = "braposas84@gmail.com";
 
-let email = 'braposas84@gmail.com';
-console.log(email);
+// let result = email.lastIndexOf('o'); // Last letter that is use on the parameter
 
-// String Concatenation
-let fName = "John";
-let lName = "Doe";
+// let result = email.slice(0, 9); // Slicing the word depends on the parameters
 
-let fullName = fName + ' ' + lName;
+// let result = email.substr(4, 10); // Same as slice
 
-console.log(fullName);
+let result = email.replace('b', 'R'); // Replacing String (only the first letter encountered)
 
-// Getting Characters
-console.log(fullName[2]); // Gets single character on a string (Counts from 0)
-
-// String Length
-console.log(fullName.length); // Number of letters on screen // This is a property NOT method
-
-// String Methods // Methods is the process of doing something to the value while the 'length' will not do something to the variable but just counts it
-console.log(fullName.toUpperCase()); // Methods have open and close parenthesis after the method name
-let result = fullName.toLocaleLowerCase(); // Storing it to a variable with a method
-console.log(result, fullName); // There are methods that can alter the original string that is in the value
-
-let index = email.indexOf('@'); // Passing a value to a method is called arguments or parameters
-console.log(index);
+console.log(result);
