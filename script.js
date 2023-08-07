ce68d80c14359d56ff4975cf1cc74c5f10bf3909
francis123 <braposas84@gmail.com>
2023-08-07 11:51:58 +0800

    JavaScript Files 6

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
-let radius = 10;
-const pi = 3.14;
+// Template strings
+const title = "Best reads of 2019";
+const author = "John";
+const likes = 30;
 
-// console.log(radius, pi);
-
-// Math oprators (+, -, *, /, **, %)
-
-// console.log(10 /2);
-// let result = radius % 3; //Remainder
-// let result = pi * radius**2
-
-// order of operation - BIDMAS - brackets, indecies, division, multiplication, addition, subtraction
-
-// let result = 5 * (10-3)**2
-
+// Concatenation way
+// let result = "The blog called " + title + " by " + author + ' has ' + likes + ' likes';
 // console.log(result);
 
-let likes = 10;
-// likes = likes + 1; //There is a shorter version for this
-// likes++; // This is the same in adding one
-// likes--; // Same goes with subtraction
-
-// Adding 10
-// likes += 10; // Add 10 (same goes with other op)
-// likes -= 5;
-// likes *= 2;
-// likes /=2;
-
-// console.log(likes);
-
-// NaN = not a number
-// console.log(5 / "hello"); // hello is NaN - not a number
-
-// Number concatenation with string
-let result = "The blog has " + likes + ' likes'
-
+// Template String Way
+let result = `The blog called ${title} by ${author} has ${likes} likes`; // Less messy way
 console.log(result);
+
+// Creating HTML templates
+let html = `
+    <h2>${title}<h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
