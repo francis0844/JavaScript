21c144081c07e6d240c42af90a5babccfde24db5
francis123 <braposas84@gmail.com>
2023-08-07 13:41:06 +0800

    JavaScript Files 19

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// Logical NOT (!)
+// Break and Continue
 
-let user = false;
+const scores = [10, 50, 0, 25, 60, 100, 20, 40];
 
-if(!user){
-    console.log('hello');
+for(let i = 0; i < scores.length; i++){
+
+    if(scores[i] === 0){
+        continue;
+    }
+
+    console.log('your score: ', scores[i]);
+
+    if(scores[i] === 100){
+        console.log('congrats, you got the top score!');
+        break;
+    }
 }
-
-console.log(!true);
-console.log(!false);
