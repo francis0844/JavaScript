ba990d3dcd9e49cf7901d41ca9224063620803ab
francis123 <braposas84@gmail.com>
2023-08-07 13:17:56 +0800

    JavaScript Files 14

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// While Loops
-const names = ['shaun', 'mario', 'luigi']
-// let i = 0;
+// Do While Loop
 
-// while(i < 5){
-//     console.log('in loop: ', i)
-//     i++;
-// }
-
-let i = 0;
-while(i < names.length){
-    console.log(names[i]);
+let i = 3;
+do{
+    console.log('val of i is: ', i);
     i++;
 }
+while(i < 5);
