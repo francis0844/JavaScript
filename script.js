52923b54e7ea17623d2f37a9ba3b63520d9bcfa9
francis123 <braposas84@gmail.com>
2023-08-07 12:08:32 +0800

    JavaScript Files 8

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-let ninjas = ['john', 'francis', 'james'];
+let age; // Undefined, not defined value
+age = null; //defined value but null
 
-//ninjas[1] = 'ken'; // Overridding values inside array
+//Null is use to clearout forms and to reset values
 
-console.log(ninjas[1]);
-
-let ages = [23, 25, 27]; // Storing Numbers on array
-console.log(ages[2]);
-
-let random = ['shaun', 'francis', 25, 20];
-console.log(random);
-
-console.log(ninjas.length); // Arrays have properties too
-
-// Array methods
-// let result = ninjas.join(','); // Comma is the parameter for separator
-
-// let result = ninjas.indexOf('francis'); // Determines the number or position of array
-
-// let result = ninjas.concat(['krystal', 'jennifer']) // Adds values to the array
-
-// let result = ninjas.push('krystal'); // Adds a value but returns length
-
-let result = ninjas.pop(); // deletes the last value and returns what was deleted
-
-console.log(ninjas);
+console.log(age, age + 3, `the age is ${age}`); // Undefined value/Null Value
