d4cfe9d4c021dcd8845fac4d99dd83c31b37134f
francis123 <braposas84@gmail.com>
2023-08-07 15:24:55 +0800

    JavaScript Files 33

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-// object literals
-let user = {
-    name: 'Crystal',
-    age: 30,
-    email: 'crystal@gmail.com',
-    location: 'berlin',
-    blogs: [
-        {
-            title: 'blog 1',
-            likes: 30
-        },
-        {
-            title: 'blog 2',
-            likes: 50
-        }
-    ],
-    login: function(){
-        console.log('the user logged in');
-    },
-    logout: function(){
-        console.log('the user logged out')
-    },
-    logBlogs: function(){
-        // console.log(this.blogs);
-        console.log('this user has written:')
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        })
-    }
-};
+// Math object
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-user.logBlogs();
+const area = 5.3;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// random num
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round(random * 100));
