ef86330511945408282ce214a32badc4eb93c204
francis123 <braposas84@gmail.com>
2023-08-07 14:33:48 +0800

    JavaScript Files 26

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-// Arrow Function
-const calcArea = (radius) => 3.14 * radius**2;
+const name = 'shaun';
 
-const area = calcArea(5);
-console.log('area is: ', area);
+// Functions
+const greet = () => 'hello';
 
-const greet = () => 'hello world';
+let resultOne = greet();
+console.log(resultOne);
 
-const result = greet();
-console.log(result);
-
-// const bill = function(products, tax) => {
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// };
-
-
-const bill = (products, tax) => {
-    let total = 0;
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-    return total;
-}
-
-console.log(bill([10, 15, 30], 0.2));
+// Methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
