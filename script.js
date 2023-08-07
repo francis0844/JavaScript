c8a69e84b8cd94e410bb4cbbd14d034c6cd84d8c
francis123 <braposas84@gmail.com>
2023-08-07 11:24:37 +0800

    JavaScript Files 3

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let age = 25; // two words that are separated with space cannot be used as a name, you can't also use variable assigner as a name, names cannot also start with number
-let year = 2019;
+// Strings
+console.log('hello, world');
 
-// This is how a comment looks like
+let email = 'braposas84@gmail.com';
+console.log(email);
 
-// Log things to console
-console.log(age, year);
+// String Concatenation
+let fName = "John";
+let lName = "Doe";
 
-age = 30;
-console.log(age);
+let fullName = fName + ' ' + lName;
 
-const points = 100; // Const cannot be overridden like how let can be overridden
-console.log(points);
+console.log(fullName);
 
-var score = 75; // Older way of creating variables
-console.log(score);
+// Getting Characters
+console.log(fullName[2]); // Gets single character on a string (Counts from 0)
 
-// Modern JavaScript is sometimes not compatible with older browsers when using let and const variable assigner
+// String Length
+console.log(fullName.length); // Number of letters on screen // This is a property NOT method
+
+// String Methods // Methods is the process of doing something to the value while the 'length' will not do something to the variable but just counts it
+console.log(fullName.toUpperCase()); // Methods have open and close parenthesis after the method name
+let result = fullName.toLocaleLowerCase(); // Storing it to a variable with a method
+console.log(result, fullName); // There are methods that can alter the original string that is in the value
+
+let index = email.indexOf('@'); // Passing a value to a method is called arguments or parameters
+console.log(index);
