79410b2918df286c8e238a4c690c4c50df757fc2
francis123 <braposas84@gmail.com>
2023-08-07 14:29:14 +0800

    JavaScript Files 25

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// Returning Values
-// const speak =  function(name = 'John', time = "day"){
-//     console.log(`good ${time} ${name}!`);
+// Arrow Function
+const calcArea = (radius) => 3.14 * radius**2;
+
+const area = calcArea(5);
+console.log('area is: ', area);
+
+const greet = () => 'hello world';
+
+const result = greet();
+console.log(result);
+
+// const bill = function(products, tax) => {
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
 // };
 
-const calcArea = function(radius){
-    return 3.14 * radius**2;
-};
 
-const a = calcArea(5);
-console.log(a);
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i < products.length; i++){
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+console.log(bill([10, 15, 30], 0.2));
