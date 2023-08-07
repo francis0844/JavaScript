63ea904dc75457fc2726c7785e350283aa414fe1
francis123 <braposas84@gmail.com>
2023-08-07 13:46:40 +0800

    JavaScript Files 20

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-// Break and Continue
+// switch
 
-const scores = [10, 50, 0, 25, 60, 100, 20, 40];
 
-for(let i = 0; i < scores.length; i++){
+// Evaluating Variable
+const grade = 'D'; //Switch case are checking very strictly
 
-    if(scores[i] === 0){
-        continue;
-    }
-
-    console.log('your score: ', scores[i]);
-
-    if(scores[i] === 100){
-        console.log('congrats, you got the top score!');
+switch(grade){
+    case 'A':
+        console.log('You got A');
         break;
-    }
+    case 'B':
+        console.log('You got B');
+        break;
+    case 'C':
+        console.log('You got C');
+        break;
+    case 'D':
+        console.log('You got D');
+        break;
+    case 'E':
+        console.log('You got E');
+        break;
+    default:
+        console.log('not a grade');
 }
