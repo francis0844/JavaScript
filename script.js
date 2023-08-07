e593f73467997068922a6280958c3cb3bf61e842
francis123 <braposas84@gmail.com>
2023-08-07 12:28:55 +0800

    JavaScript Files 9

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-let age; // Undefined, not defined value
-age = null; //defined value but null
+// Booleans
+// console.log(true, false, "true", "false"); // Two different data types - boolean and string
 
-//Null is use to clearout forms and to reset values
+// Methods can return booleans
+let email = 'braposas84@gmail.com';
+let names = ['mario', 'luigi', 'toad']
 
-console.log(age, age + 3, `the age is ${age}`); // Undefined value/Null Value
+// let result = email.includes('!'); // Checks if there are character that is included
+// let result = names.includes('luigi'); // Checks if the parameter is inside the array
+
+// console.log(result);
+
+// Comparison operators
+
+let age = 25;
+
+// console.log(age == 25); // Checks if it is equal with the variable
+// console.log(age == 30); // Checks if it is equal with the variable
+// console.log(age != 30); // Checks if it is not equal with the variable
+// console.log(age > 20); // Checks if it is less than with the variable
+// console.log(age < 20); // Checks if it is greater than with the variable
+// console.log(age >= 20); // Checks if it is less than equals with the variable
+// console.log(age <= 20); // Checks if it is greater than equals with the variable
+
+let name = 'john';
+
+console.log(name == 'john'); // Equal
+console.log(name == 'John'); // !Equal
+console.log(name > 'crystal'); // first letter J comes later than C (first letter position)
+console.log(name > 'John'); // lowercase are greater than uppercase letter
+console.log(name > 'Crystal'); // lowercase is greater and j comes later than c
