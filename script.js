756f4e0537adbbda2c2e063db2e64bad96647b6f
francis123 <braposas84@gmail.com>
2023-08-07 12:48:24 +0800

    JavaScript Files 10

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-// Booleans
-// console.log(true, false, "true", "false"); // Two different data types - boolean and string
-
-// Methods can return booleans
-let email = 'braposas84@gmail.com';
-let names = ['mario', 'luigi', 'toad']
-
-// let result = email.includes('!'); // Checks if there are character that is included
-// let result = names.includes('luigi'); // Checks if the parameter is inside the array
-
-// console.log(result);
-
-// Comparison operators
-
 let age = 25;
 
-// console.log(age == 25); // Checks if it is equal with the variable
-// console.log(age == 30); // Checks if it is equal with the variable
-// console.log(age != 30); // Checks if it is not equal with the variable
-// console.log(age > 20); // Checks if it is less than with the variable
-// console.log(age < 20); // Checks if it is greater than with the variable
-// console.log(age >= 20); // Checks if it is less than equals with the variable
-// console.log(age <= 20); // Checks if it is greater than equals with the variable
+// Comparisons even in different data types
 
-let name = 'john';
+// console.log(age == 25);
+// console.log(age == "25");
+// console.log(age != 25);
+// console.log(age != "25");
 
-console.log(name == 'john'); // Equal
-console.log(name == 'John'); // !Equal
-console.log(name > 'crystal'); // first letter J comes later than C (first letter position)
-console.log(name > 'John'); // lowercase are greater than uppercase letter
-console.log(name > 'Crystal'); // lowercase is greater and j comes later than c
+// strict comparison
+console.log(age === 25); //Triple equals shows strict comparison
+console.log(age === "25"); //false
+console.log(age !== 25); //Triple !equals shows strict comparison
+console.log(age !== "25"); //true, because they are not equal
