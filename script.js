9dc2a910071624043be4a74a76dfed5e079f49e6
francis123 <braposas84@gmail.com>
2023-08-07 15:58:05 +0800

    DOM 2

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Query selectors
+// Get element by ID
+const title = document.getElementById('title');
+console.log(title);
 
-// const para = document.querySelector('div.error');
-
-// console.log(para);
-
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-
-paras.forEach(para => {
-    console.log(para);
-});
-
-console.log(paras[0]);
+// Get element by class
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+
+// Get element by tag
+const paras = document.getElementsByTagName('p');
+console.log(paras[0]);
